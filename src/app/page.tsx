6f2a87f2393ac32267a0e881a3045d8054bf7195
Bruno Arouca <brunoarouca.dev@gmail.com>
2023-10-16 21:59:03 -0300

    Modulo III - até a parte de arrat em State: Alterar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,71 @@
+
 import Image from 'next/image'
-import { StarRating } from '../components/review/StarRating'
-import StudentTable from '@/components/StudentTable';
-import {students} from '../data/students';
+import { TodoItem } from '../types/TodoItem';
+'use client'
+import { useState } from 'react';
+
 
 export default function Home() {
-  return (
-    <main className='bg-white'>
-      <StarRating rate={4.4} />
+  const [itemInput, setItemInput] = useState('');
 
-      <StudentTable data= {students}/>
-    </main>
+  const [list, setList] = useState<TodoItem[]>([
+    { id: 1, label: 'Fazer dever de casa', checked: true},
+    { id: 2, label: 'Comprar bolo', checked: false}
+
+  ]);
+
+  function handleClick(){
+    if(itemInput.trim() === '') return
+    setList([...list, {id: list.length + 1 ,label: itemInput, checked: false}]);
+    setItemInput('')
+  }
+
+  function removerItem(id: number){
+    setList(list.filter( (todo) => todo.id !== id))
+  }
+
+  function toggleItem(id: number){
+    let newList = [...list];
+
+    newList.forEach((todo) => {
+      if(todo.id === id){
+        todo.checked = !todo.checked
+      }
+    })
+    // newList[item].checked = !newList[item].checked;
+    
+    setList(newList)
+  }
+
+  return (
+    <div className='w-screen h-screen flex flex-col items-center text-2xl bg-black'>
+      <h1 className='text-4xl mt-5'>Lista de tarefas</h1>
+
+      <div className='flex w-full max-w-lg my-3 p-4 rounded-md bg-gray-700 border-2 border-gray-800'>
+        <input 
+          type="text"
+          placeholder='O que deseja fazer?'
+          className='flex-1 p-3 text-2xl text-black'
+          value={itemInput}
+          onChange={e => setItemInput(e.target.value)}
+        />
+
+        <button className='text-gray-200 ml-3'onClick={handleClick} >Adicionar</button>
+      </div>
+
+      <p className='text-gray-100 mb-8'> {list.length} itens na lista </p>
+
+      <ul className='w-full max-w-lg list-disc pl-5 text-gray-200'>
+
+        { list.map( (item) => (
+          
+          <li key={item.id} className={item.checked ? 'line-through' : ''} >
+            <input onClick={() => toggleItem(item.id)} type="checkbox" checked={item.checked} className='w-6 h-6 mr-3' />
+
+            {item.label} - <button onClick={() => removerItem(item.id)} className='hover:underline'>[ deletar ]</button>
+          </li>
+        )) }
+      </ul>
+    </div>
   )
 }
--- src/components/StudentTable/index.tsx
+++ src/components/StudentTable/index.tsx
@@ -1,11 +1,15 @@
 import { Student } from "@/types/Student";
 
-const StudentTable = (students: Student[]) => {
+interface StudentTableProps {
+  dataStudents: Student[];
+}
+
+const StudentTable = ({dataStudents}: StudentTableProps) => {
   return (
     <>
-      <table className="table-fixed border-collapse border border-slate-500 max-w-4xl">
-        <thead className="bg-gray-700 text-gray-100 font-normal">
-          <tr>
+      <table className="table-fixed border-collapse border border-slate-500 max-w-4xl rounded">
+        <thead className="bg-gray-700 text-gray-100 font-normal rounded" >
+          <tr className="rounded">
             <th className="border border-slate-600 w-3/5">Name</th>
             <th className="border border-slate-600 w-1/5">Status</th>
             <th className="border border-slate-600 px-4" >Grade1</th>
@@ -13,15 +17,15 @@ const StudentTable = (students: Student[]) => {
             <th className="border border-slate-600 w-2/5">Final Grade</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="rounded">
           {
-            students?.map(student => (
+            dataStudents?.map(student => (
               <tr key={student.id}>
                 <td className="border border-slate-700 "> {student.name} </td>
-                <td className="border border-slate-700">{student.active}</td>
+                <td className="border border-slate-700">{student.active ? 'Active' : 'Inactive'}</td>
                 <td className="border border-slate-700">{student.grade1}</td>
                 <td className="border border-slate-700">{student.grade2}</td>
-                <td className="border border-slate-700">{ (student.grade1 + student.grade2) / 2 }</td>
+                <td className="border border-slate-700">{ ((student.grade1 + student.grade2) / 2).toFixed(2) }</td>
               </tr>
             ))
           }
